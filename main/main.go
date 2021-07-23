9afd345abf4d2e02cbf5302c95d1baf80046d1b8
Chunxu Zou <471500439@qq.com>
2021-07-23 23:25:23 +0800

    upd

--- main/main.go
+++ main/main.go
@@ -17,6 +17,16 @@ func (f Foo) Sum(args Args, reply *int) error {
 	return nil
 }
 
+func (f Foo) Sub(args Args, reply *int) error {
+	*reply = args.Num1 - args.Num2
+	return nil
+}
+
+func (f Foo) Times(args Args, reply *int) error {
+	*reply = args.Num1 * args.Num2
+	return nil
+}
+
 func startServer(addr chan string) {
 	var foo Foo
 	if err := myrpc.Register(&foo); err != nil {
@@ -48,7 +58,7 @@ func main() {
 			defer wg.Done()
 			args := &Args{Num1: i, Num2: i * i}
 			var reply int
-			if err := client.Call("Foo.Sum", args, &reply); err != nil {
+			if err := client.Call("Foo.Sub", args, &reply); err != nil {
 				log.Fatal("call Foo.Sum error:", err)
 			}
 			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
