d4512cf682b80267b16088ef4c9bc491256fa686
Chunxu Zou <471500439@qq.com>
2021-07-26 23:27:34 +0800

    gob test

---
+++ tests/gob_test.go
@@ -0,0 +1,34 @@
+package tests
+
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"testing"
+)
+
+type demo struct {
+	H string
+}
+
+func TestGobT(t *testing.T) {
+	b := &bytes.Buffer{}
+	w := gob.NewEncoder(b)
+	r := gob.NewDecoder(b)
+
+	var i int
+	i = 10
+	_ = w.Encode(&i)
+	d := &demo{
+		H: "hh",
+	}
+	_ = w.Encode(d)
+	var x int
+	var dd demo
+	_ = r.Decode(&x)
+
+	_ = r.Decode(&dd)
+
+	fmt.Println(x)
+	fmt.Println(dd.H)
+}
