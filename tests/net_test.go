cd47af862086ec5446835097cfd0fba5eda0f50a
Chunxu Zou <471500439@qq.com>
2021-08-03 00:20:49 +0800

    conn

--- tests/net_test.go
+++ tests/net_test.go
@@ -2,26 +2,50 @@ package tests
 
 import (
 	"encoding/gob"
+	"fmt"
 	"net"
+	"sync"
 	"testing"
 )
 
-type header struct {
-	serviceName string
-	args        interface{}
+type Header struct {
+	ServiceName string
+	Args        interface{}
 }
 
-func TestHello(t *testing.T) {
-
+func TestConn(t *testing.T) {
+	ch := make(chan struct{})
 	//server
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		l, _ := net.Listen("tcp", ":9999")
+		ch <- struct{}{}
 		conn, _ := l.Accept()
 		decoder := gob.NewDecoder(conn)
-		h := header{}
-		decoder.Decode(&h)
+		sh := Header{}
+		decoder.Decode(&sh)
+		fmt.Printf("h: %+v\n", sh)
 	}()
-
+	<-ch
 	//client
+	dial, err := net.Dial("tcp", ":9999")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	h := Header{
+		ServiceName: "h.n",
+		Args:        "aa",
+	}
+	encoder := gob.NewEncoder(dial)
+	err = encoder.Encode(h)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	wg.Wait()
 
 }
